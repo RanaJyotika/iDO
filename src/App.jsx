7012ed3568e8d5cac8c6434ccbc1e046c9614766
RanaJyotika <srrana956@gmail.com>
2025-02-19 15:36:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+    <div className='justify-center items-center flex h-screen'>
+
+      <h1 className='text-center text-red-700 font-mono text-6xl '>coming soon...</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
